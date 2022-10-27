a2d8546a0358a3bb9887f3eb76e7f8503f2691c5
Ilya Ivanov <static.ila@gmail.com>
2022-10-27 20:18:44 +0300

    Extract item creation

--- main.c
+++ main.c
@@ -9,9 +9,6 @@ int isRunning = 0;
 
 void handleEvents();
 
-SDL_Texture *text_texture;
-SDL_Rect text_dest = {0};
-
 SDL_Color fontColor = {255, 255, 255};
 SDL_Color bg = {30, 30, 30};
 SDL_Rect screenDimensions = {0, 0, 512, 512};
@@ -24,40 +21,71 @@ int squareLeftRightMargin = 7;
 int squareSize = 8;
 SDL_Color squareColor = {255, 255, 255};
 
+struct ItemView
+{
+	// top left corner of a squre in an item
+	int x, y;
+	int textHeight;
+	int textWidth;
+	char *text;
+	SDL_Texture *textTexture;
+};
+
+struct ItemView *view;
+struct ItemView *view2;
+
+TTF_Font *segoefont;
+int ascent;
+
+void renderItem(struct ItemView *itemView)
+{
+	// draw rect
+	SDL_SetRenderDrawColor(renderer, squareColor.r, squareColor.g, squareColor.b, 255);
+	SDL_Rect rectPosition = {itemView->x, itemView->y, squareSize, squareSize};
+	SDL_RenderFillRect(renderer, &rectPosition);
+
+	// draw text
+	SDL_Rect text_dest = {0};
+	text_dest.x = itemView->x + squareLeftRightMargin + squareSize;
+	text_dest.y = itemView->y - (ascent / 2);
+	text_dest.w = itemView->textWidth;
+	text_dest.h = itemView->textHeight;
+	SDL_RenderCopy(renderer, itemView->textTexture, NULL, &text_dest);
+}
+
 void draw()
 {
 	SDL_SetRenderDrawColor(renderer, bg.r, bg.g, bg.b, 255);
 	SDL_RenderFillRect(renderer, &screenDimensions);
 
-	SDL_SetRenderDrawColor(renderer, squareColor.r, squareColor.g, squareColor.b, 255);
-	SDL_Rect rectPosition = {left + squareLeftRightMargin, top, squareSize, squareSize};
-	SDL_RenderFillRect(renderer, &rectPosition);
-
-	SDL_RenderCopy(renderer, text_texture, NULL, &text_dest);
+	renderItem(view);
+	renderItem(view2);
 
 	SDL_RenderPresent(renderer);
 }
 
+struct ItemView *createItem(int x, int y, char *textChars)
+{
+	struct ItemView *res = malloc(sizeof(struct ItemView));
+	res->x = x;
+	res->y = y;
+
+	res->text = textChars;
+	SDL_Surface *text = TTF_RenderText_LCD(segoefont, res->text, fontColor, bg);
+	res->textHeight = text->h;
+	res->textWidth = text->w;
+	res->textTexture = SDL_CreateTextureFromSurface(renderer, text);
+	SDL_FreeSurface(text);
+	return res;
+}
+
 void init()
 {
-	TTF_Font *segoefont = TTF_OpenFont("fonts/seguisb.ttf", 16);
+	segoefont = TTF_OpenFont("fonts/seguisb.ttf", 16);
+	ascent = TTF_FontAscent(segoefont);
 
-	SDL_Color txtBg = bg;
-	SDL_Surface *text = TTF_RenderText_LCD(segoefont, "Electro", fontColor, txtBg);
-
-	int ascent = TTF_FontAscent(segoefont);
-
-	text_texture = SDL_CreateTextureFromSurface(renderer, text);
-
-	text_dest.x = left + squareLeftRightMargin * 2 + squareSize;
-	// text_dest.y = top - ((text->h - squareSize) / 2);
-	text_dest.y = top - (ascent / 2);
-	text_dest.w = text->w;
-	text_dest.h = text->h;
-
-	TTF_CloseFont(segoefont);
-
-	SDL_FreeSurface(text);
+	view = createItem(left, top, "Electro");
+	view2 = createItem(left, top + inBetweenItems+squareSize, "Ambient");
 }
 
 int main(int argc, char *argv[])
@@ -83,8 +111,12 @@ int main(int argc, char *argv[])
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 
+	SDL_DestroyTexture(view->textTexture);
+
 	TTF_Quit();
 	SDL_Quit();
+
+	free(view);
 
 	return 0;
 }
